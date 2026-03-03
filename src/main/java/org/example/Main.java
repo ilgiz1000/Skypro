f8e2c273561def941b342a56663a1c82121457f1
ARDOR GAMING <git@github.com:ilgiz1000/2026-03-03.git>
2026-03-03 05:44:25 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,39 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        var dog = 8.0;
+        dog = dog + 4;
+        dog = dog - 3.5;
+        System.out.println (dog);
+        var cat = 3.6;
+        cat = 3.6 + 4;
+        cat = cat + 1.6;
+        System.out.println (cat);
+        var paper = 763789;
+        paper = paper + 4;
+        paper = paper + 7639;
+        System.out.println (paper);
+        var frog = 3.5;
+        System.out.println (frog);
+        frog = frog*10;
+        System.out.println (frog);
+        frog = frog/3.5;
+        frog = frog+4;
+        System.out.println (frog);
+        var x = 78.2;
+        var y = 82.7;
+        var summ = x+y;
+        var diff = y-x;
+        System.out.println (summ);
+        System.out.println (diff);
+        var ost = y%x;
+        System.out.println (ost);
+        var general = 640;
+        var work = 8;
+        var number = general / work;
+        System.out.println ("Всего работников в компании - " + number + " человек");
+        var number_2 = number + 94;
+        var newGeneral = work * number_2;
+        System.out.println ("Если в компании работает " + number_2 + " человек, то всего " + newGeneral + " часов работы может быть поделено между сотрудниками");
         }
     }
-}
